5eb8794277181706c4d296c17c4f8efdb2568011
CodebyChris07 <chrisjalu07@gmail.com>
2025-10-29 12:06:24 -0500

    println y print

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -18,5 +18,13 @@ public class Ejemplo04 {
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
-    }
+        // print no hace enter println si
+        
+        System.out.println("-------------");
+        System.out.println(nombreEstudiante + " ");
+        System.out.println(apellidoEstudiante + " ");
+        System.out.println(nacimiento + " ");
+        System.out.println(nombreEstudiante + " ");
+        
+}
 }
